37a9d89a30cf936b08846fa235bd293cecab86a4
RickySulli <71475646+RickySulli@users.noreply.github.com>
2020-12-17 02:10:58 -0600

    tables working

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
-CREATE TABLE canidates(
+CREATE TABLE candidates(
 id INTEGER PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-industry_connected BOOLEAN NOT NULL)
+industry_connected BOOLEAN NOT NULL
+);
