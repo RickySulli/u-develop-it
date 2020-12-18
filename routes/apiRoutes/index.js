2e80e1ba0244cb5587ff2009f66366e7b2318984
RickySulli <71475646+RickySulli@users.noreply.github.com>
2020-12-18 12:10:15 -0600

    attempting post and put requests on voterRoutes.js

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.use(require('./candadateRoutes'));
+router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
+
+
+
+
+module.exports = router;
